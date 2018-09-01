ef8038818537bb5581f00d672a10a9f5984a8710
jojoldu <jojoldu@gmail.com>
2018-09-01 17:40:06 +0900

    profile 체크 코드 및 테스트 코드 추가

---
+++ src/main/java/com/jojoldu/book/travis/ApiController.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.travis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jojoldu@gmail.com on 01/09/2018
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RequiredArgsConstructor
+@RestController
+public class ApiController {
+    private final Environment env;
+
+    @GetMapping("/activeProfile")
+    public String activeProfile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+
+        if(profiles.contains("set1")){
+            return "set1";
+        }
+
+        if(profiles.contains("set2")){
+            return "set2";
+        }
+
+        return profiles.get(0);
+    }
+}
---
+++ src/test/java/com/jojoldu/book/travis/ApiControllerTest.java
@@ -0,0 +1,39 @@
+package com.jojoldu.book.travis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by jojoldu@gmail.com on 01/09/2018
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles(profiles = {"set1", "real-db", "real-cache"})
+public class ApiControllerTest {
+
+    @Autowired
+    private ApiController apiController;
+
+    @Test
+    public void 여러profile이있어도_set1_set2를_조회한다() {
+        //given
+        String expectedProfile = "set1";
+
+        //when
+        String profile = apiController.activeProfile();
+
+        //then
+        assertThat(expectedProfile, is(profile));
+
+    }
+}
