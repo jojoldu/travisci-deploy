94b3368f006a641d25349a90602d6e974b82f2fc
jojoldu <jojoldu@gmail.com>
2018-09-06 09:26:34 +0900

    code deploy도 연동

---
+++ code-deploy/start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "  > Jar 복사를 시작합니다."
+echo "  > Jar를 모아놓는 디렉토리를 생성합니다. 있으면 생성하지 않습니다."
+jar_dir_path=/home/ec2-user/jar/
+mkdir -p ${jar_dir_path}
+
+echo "  > Jar 이름을 찾습니다."
+jar_name=$(basename /home/ec2-user/deploy/*.jar)
+echo "  > Jar 이름: $jar_name"
+
+echo "  > Jar 파일을 ${jar_dir_path}로 복사합니다."
+cp /home/ec2-user/deploy/*.jar ${jar_dir_path}
+jar_deploy_path=${jar_dir_path}${jar_name}
+
+RUN_COMMAND="nohup java -jar ${jar_deploy_path} >> /home/ec2-user/app/nohup.out 2>&1&"
+
+echo "  > ${RUN_COMMAND}"
+${RUN_COMMAND}
--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-RUN_COMMAND="nohup java -jar ${application_path} >> /home/ec2-user/app/nohup.out 2>&1&"
-
-echo "  > ${RUN_COMMAND}"
-${RUN_COMMAND}
