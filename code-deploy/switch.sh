259ed6547de4d7e438cc15c82dae869c2e127523
jojoldu <jojoldu@gmail.com>
2018-09-07 19:13:57 +0900

    switch 코드 정리

--- code-deploy/switch.sh
+++ code-deploy/switch.sh
@@ -4,32 +4,14 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-function switch()
-{
-    idle_port=$(find_idle_port)
+idle_port=$(find_idle_port)
 
-    echo "> 전환할 Port: $idle_port"
-    echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:${idle_port};" |sudo tee /etc/nginx/conf.d/service-url.inc
+echo "> 전환할 Port: $idle_port"
+echo "> Port 전환"
+echo "set \$service_url http://127.0.0.1:${idle_port};" |sudo tee /etc/nginx/conf.d/service-url.inc
 
-    current_profile=$(curl -s http://localhost/profile)
-    echo "> Nginx Current Profile : $current_profile"
+current_profile=$(curl -s http://localhost/profile)
+echo "> Nginx Current Profile : $current_profile"
 
-    echo "> Nginx Reload"
-    sudo service nginx reload
-
-    checkSwitch
-}
-
-function checkSwitch()
-{
-    response=$(curl -s http://localhost/actuator/health)
-    up_count=$(echo ${response} | grep 'UP' | wc -l)
-    if [ ${up_count} -eq 0 ]
-    then # $up_count == 0 ("UP" 문자열이 없으면)
-        echo "> Switch 실패"
-        exit 1
-    fi
-}
-
-switch
+echo "> Nginx Reload"
+sudo service nginx reload
